a37af3ed3b02d68229892ebafc1b1d5130bbf834
Siena Rivera <sienasrivera@gmail.com>
2024-04-11 12:47:43 -0700

    Homepage!!

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+    }).then(() => jsConfetti.addConfetti())
+})
