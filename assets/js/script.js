33499f14f35ec0afabe5fd73d78d3290ef5e668c
Siena Rivera <sienasrivera@gmail.com>
2024-04-12 07:25:30 -0700

    CONFETTIIIII

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 /* jshint esversion: 6 */
-/* import JSConfetti from "js-confetti"; */
-
+import JSConfetti from "https://esm.sh/js-confetti"; 
 const canvas = document.getElementById('confetti');
 const button = document.getElementById('button');
 
-const jsConfetti = new JSConfetti({canvas})
+const jsConfetti = new JSConfetti({canvas});
 
 button.addEventListener('click', () => {
   jsConfetti.addConfetti({
     emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
   }).then(() => jsConfetti.addConfetti())});
+
